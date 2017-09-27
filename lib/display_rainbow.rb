10964a7f40d3ae292a81fddb5d930c466235be0d
Larissa S <larissafschiavo@gmail.com>
2017-09-27 16:25:37 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,12 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   ans = []
-  colors.each {|color|
+  colors.each_with_index {|color, idx|
     capletter = color.upcase.split
     c = capletter[0][0]
     ans << "#{c}: #{color} "
+    if idx != colors.length -1 
+      ans << ","
   }
-  puts ans.join 
+  puts ans.join
 end
