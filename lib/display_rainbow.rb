46265e28c935b1f66d4f2cff6ff368f1057c3081
Larissa S <larissafschiavo@gmail.com>
2017-09-27 16:19:40 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,9 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  ans = []
+  colors.each {|color|
+    capletter = color.split.first.upcase
+    ans << "#{capletter}: #{color}"
+  }
+  return ans.join
+end
